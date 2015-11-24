565cb277988fe07ffb872f016b61c10f5d9d95b0
Riccardo Binetti <rbino@gmx.com>
2015-11-24 21:42:08 +0100

    Added device command line option

--- gh-midi.go
+++ gh-midi.go
@@ -2,11 +2,12 @@ package main
 
 import (
     "fmt"
+    "flag"
     "github.com/gvalkov/golang-evdev"
 )
 
-func ReadGuitar(c chan *evdev.InputEvent){
-  guit, _ := evdev.Open("/dev/input/by-id/usb-0810_Twin_USB_Joystick-event-joystick")
+func ReadGuitar(c chan *evdev.InputEvent, dev string){
+  guit, _ := evdev.Open(dev)
   for {
     ev, _ := guit.ReadOne()
     if ev.Type == evdev.EV_KEY || ev.Type == evdev.EV_ABS {
@@ -16,8 +17,11 @@ func ReadGuitar(c chan *evdev.InputEvent){
 }
 
 func main() {
+  dev := flag.String("d", "/dev/input/by-id/usb-0810_Twin_USB_Joystick-event-joystick", "The event device associated with the GH controller")
+  flag.Parse()
+
   c := make(chan *evdev.InputEvent)
-  go ReadGuitar(c)
+  go ReadGuitar(c, *dev)
   for {
     but := <- c
     fmt.Println(but.Code, but.Value)
